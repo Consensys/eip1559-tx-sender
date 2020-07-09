b039b803cdfa92dec01c0d1baab0902564e56998
Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>
2020-07-09 14:05:50 +0200

    Update EIP1559TransactionCreator.java
    set gaspremium to 1 GWEI

--- src/main/java/tech/pegasys/net/api/service/transaction/EIP1559TransactionCreator.java
+++ src/main/java/tech/pegasys/net/api/service/transaction/EIP1559TransactionCreator.java
@@ -1,9 +1,11 @@
 package tech.pegasys.net.api.service.transaction;
 
-import java.math.BigInteger;
-
+import org.web3j.utils.Convert;
 import tech.pegasys.net.api.model.EIP1559Transaction;
 import tech.pegasys.net.api.model.payload.TransactionPayload;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
 
 public interface EIP1559TransactionCreator {
 
@@ -12,6 +14,9 @@ public interface EIP1559TransactionCreator {
   EIP1559Transaction create(BigInteger nonce, BigInteger gasPremium, BigInteger feeCap);
 
   default EIP1559Transaction create(BigInteger nonce) {
-    return create(nonce, BigInteger.valueOf(2000001529), BigInteger.valueOf(999999999));
+    return create(
+        nonce,
+        Convert.Unit.GWEI.getWeiFactor().multiply(BigDecimal.ONE).toBigInteger(),
+        BigInteger.valueOf(1999999999));
   }
 }

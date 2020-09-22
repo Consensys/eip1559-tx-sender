c3313f4d10e75940efb15c0d43f50c5766feffeb
Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>
2020-09-22 17:36:08 +0200

    init test framework

---
+++ src/test/java/tech/pegasys/net/core/account/AccountProcessorServiceTest.java
@@ -0,0 +1,11 @@
+package tech.pegasys.net.core.account;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AccountProcessorServiceTest {
+  @Test
+  public void nominalCase() {}
+}
